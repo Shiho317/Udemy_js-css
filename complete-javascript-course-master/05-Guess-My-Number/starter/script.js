ddf3a1b0c127e7cfcef1df0a9ae4d5beda5867a3
Shiho Nagano <shiho@nagano.com>
2021-09-22 16:32:26 -0700

    add modal starter js

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,71 @@
 'use strict';
+
+let secretNumber = Math.trunc(Math.random()*20)+1;
+
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function(message){
+  document.querySelector('.message').textContent = message;
+}
+
+document.querySelector('.check').addEventListener('click', function(){
+  const guess = Number(document.querySelector('.guess').value);
+
+  if(!guess){
+    displayMessage('Not text yet.');
+  }
+  else if(guess === secretNumber){
+    displayMessage('🎉Correct Number!');
+    
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#fff';
+    document.querySelector('.number').style.width = '30rem';
+      
+      if(score > highScore){
+    highScore = score;
+    document.querySelector('.highscore').textContent = highScore;
+      }
+  }
+
+  else if(guess > secretNumber){
+    displayMessage('Too high!');
+
+    score--;
+    document.querySelector('.score').textContent = score;
+
+    if(score < 1){
+      displayMessage('👾Game Over!!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+  else if (guess < secretNumber){
+    displayMessage('Too low!');
+
+    score--;
+    document.querySelector('.score').textContent = score;
+
+    if(score < 1){
+      displayMessage('👾Game Over!!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function(){
+  score = 20;
+  document.querySelector('.score').textContent = score;
+
+  document.querySelector('.guess').value = '';
+
+  secretNumber = Math.trunc(Math.random()*20)+1;
+
+  displayMessage('Start guessing...');
+
+  document.querySelector('.number').textContent = '?';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
+
--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+console.log('hello js');
+
+const modalBtn = document.querySelector('.modal');
+const showModalBtn = document.querySelectorAll('.show-modal');
+const overlay = document.querySelector('.overlay');
+const closeBtn = document.querySelector('.close-modal');
+
+const openModal = function(){
+  modalBtn.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for(let i = 0; i < showModalBtn.length; i++){
+  showModalBtn[i].addEventListener('click', openModal)
+};
+
+const closeModal = function(){
+  modalBtn.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+closeBtn.addEventListener('click', closeModal)
+
+overlay.addEventListener('click', closeModal)
+
+modalBtn.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', function(events){
+
+  if(events.key === ' ' && !modalBtn.classList.contains('hidden')){
+      closeModal();
+  }
+});
