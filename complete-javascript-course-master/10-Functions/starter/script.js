c86002c88288dfc7807fdb7de3613e25078eb258
Shiho Nagano <shiho@nagano.com>
2021-09-27 19:50:12 -0700

    add task10  codingchallenge 1

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const greet = (greeting) =>{
+  return (name) =>{
+    console.log(`${greeting}${name}`);
+  };
+};
+
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Steven');
+
+greet('Hello')('Jonas');
+
+const addTaxRate = function(rate){
+  return function(value){
+    return value + value * rate;
+  };
+};
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
---
+++ complete-javascript-course-master/codingchallenge/task10/main.js
@@ -0,0 +1,36 @@
+'use strict';
+
+
+const poll = {
+  question: prompt('What is your favourite programming language?'),
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+
+  answers: new Array(4).fill(0),
+
+  registerNewAnswer(){
+    const answer = Number(prompt(`${this.question}\n${this.options.join('\n')}\n('Write option number')`
+    )
+    );
+    
+    
+    typeof answer === 'number' && answer < this.answers.length && this.answers[answer]++;
+
+    console.log(this.answers);
+    this.displayResult();
+    this.displayResult('string');
+  },
+
+  displayResult(type = 'array'){
+    if(type === 'array'){
+      console.log(this.answers);
+    }else if(type === 'string'){
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  }
+};
+
+
+
+document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResult.call({answers: [5, 2, 3]},'string');
